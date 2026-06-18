425f765ddf2ad947d3a770d25af7d8420d42d159
𝕷𝖀𝕾𝕿┊𝐃𝐄𝐕 ⽮ <valerylusty@gmail.com>
2026-06-17 22:31:29 -0500

    Add Config.sh for loading settings and color codes

---
+++ core/Config.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Load settings
+source config/settings.conf
+
+# COLORS
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
